3d2682eb7202b74662709cd5cf2664d06385ba83
shubh <shubhamdixit863@gmail.com>
2024-02-25 21:15:08 +0530

    call the get api

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,8 +1,37 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
+import axios  from 'axios'; // es6 imports
+
+const Api="http://ubuntu@ec2-13-58-234-56.us-east-2.compute.amazonaws.com:8080"
 
 const Dashboard = () => {
+    const [state,setState]=useState([]);
+
+
+    useEffect(()=>{
+
+        // this shows the mounting behaviours
+
+        axios.get(Api).then(res=>{
+            console.log(res.data);
+            setState(res.data.data)
+           
+      
+          }).catch(err=>{
+            console.log(err);
+          })
+
+    },[])
+
   return (
-    <div>Dashboard</div>
+    <div>
+     {
+        state.map(ele=>(
+            <li>{ele.name}</li>
+        ))
+     }
+
+
+    </div>
   )
 }
 
